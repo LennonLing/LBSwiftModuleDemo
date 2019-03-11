9d4353fd6ba76c6ea6177aca912905e485ec40f9
lingbin <bin.ling.2018@gmail.com>
2019-03-11 21:06:11 +0800

    tijiao

---
+++ Example/Pods/Target Support Files/LBSwiftModuleDemo/LBSwiftModuleDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LBSwiftModuleDemo : NSObject
+@end
+@implementation PodsDummy_LBSwiftModuleDemo
+@end
---
+++ LBSwiftModuleDemo/Classes/Tool.swift
@@ -0,0 +1,15 @@
+//
+//  Tool.swift
+//  222
+//
+//  Created by 凌斌 on 2019/3/11.
+//  Copyright © 2019 lingbin. All rights reserved.
+//
+
+import UIKit
+
+class Tool: NSObject {
+    func log() {
+        print("log")
+    }
+}
